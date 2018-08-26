ebe195b6616266d049a7a107514cff30f517172a
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2018-08-26 20:59:55 +0700

    Update boost Windows build; add init boost

--- boost/build_install_boost.cmd
+++ boost/build_install_boost.cmd
@@ -1,9 +1,4 @@
 @echo off
-call bootstrap.bat
-SET ZLIB_PATH=C:\Projects\zlib-1.2.11
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost link=static runtime-link=static address-model=32 debug release install
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost link=static runtime-link=shared address-model=32 debug release install
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost link=shared runtime-link=shared address-model=32 debug release install
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=static address-model=32 debug release install
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=shared address-model=32 debug release install
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=shared runtime-link=shared address-model=32 debug release install
+#call bootstrap.bat
+SET ZLIB_PATH=C:\Users\Yuri\Projects\zlib-1.2.11
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --build-type=complete address-model=32,64 debug release install
---
+++ git/setup_git.cmd
@@ -0,0 +1,6 @@
+@echo off
+git config --global user.name "Yurii Cherkasov"
+git config --global user.email "yuri.cherkasov.accounts@gmail.com"
+git config --global core.autocrlf false
+git config --global core.filemode false
+git config --global branch.autosetuprebase always
