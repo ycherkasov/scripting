09244b585ab3cabe2de18ce8e5eb92354f84e807
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2018-09-20 16:54:00 +0700

    build boosy with python

---
+++ boost/build_python32.cmd
@@ -0,0 +1,4 @@
+SET ZLIB_PATH=C:\Projects\zlib-1.2.11
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=static address-model=32 debug release install
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=shared address-model=32 debug release install
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=shared runtime-link=shared address-model=32 debug release install
