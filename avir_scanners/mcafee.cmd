0461b71ee46a52a431c4bd793ed7c7c070e6beb3
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-02-04 14:43:36 +0700

    posrtable antivirus scanners

---
+++ avir_scanners/mcafee.cmd
@@ -0,0 +1,3 @@
+REM Intel McAfee GetSusp
+REM https://www.mcafee.com/enterprise/en-gb/downloads/free-tools/getsusp.html
+getsusp64.exe --SILENT
---
+++ avir_scanners/msscanner.cmd
@@ -0,0 +1,3 @@
+REM Microsoft Safety Scanner
+REM https://docs.microsoft.com/en-us/windows/security/threat-protection/intelligence/safety-scanner-download
+msert.exe /Q /F
