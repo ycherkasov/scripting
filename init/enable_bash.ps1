94252095d6dc7fc6205dfa8879204e191171c8ef
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-03-14 16:36:43 +0700

    Enable Linux subsystem

---
+++ init/enable_bash.ps1
@@ -0,0 +1,3 @@
+# Other notes on Linux subsystem om Windows 10
+# https://docs.microsoft.com/en-us/windows/wsl/install-win10
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
