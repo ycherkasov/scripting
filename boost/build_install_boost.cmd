2bd6e478080890ccce5c3da3daea4e14131d1c6f
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-03-30 20:32:20 +0700

    Without MPI

--- boost/build_install_boost.cmd
+++ boost/build_install_boost.cmd
@@ -1,4 +1,4 @@
 @echo off
 #call bootstrap.bat
 SET ZLIB_PATH=C:\Users\Yuri\Projects\zlib-1.2.11
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --build-type=complete toolset=msvc-14.0 address-model=32,64 debug release install
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --without-mpi --build-type=complete toolset=msvc-14.0 address-model=32,64 debug release install
