0865a9c7fee4eace5ea7e29330870539286a6844
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-03-30 20:30:46 +0700

    VC++ 14.0

--- boost/build_install_boost.cmd
+++ boost/build_install_boost.cmd
@@ -1,4 +1,4 @@
 @echo off
 #call bootstrap.bat
 SET ZLIB_PATH=C:\Users\Yuri\Projects\zlib-1.2.11
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --build-type=complete address-model=32,64 debug release install
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --build-type=complete toolset=msvc-14.0 address-model=32,64 debug release install
