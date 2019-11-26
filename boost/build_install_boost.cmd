e73b62a38fe68bb304fae5fdf0f5a539cdcf98c6
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-11-26 20:12:58 +0700

    x

--- boost/build_install_boost.cmd
+++ boost/build_install_boost.cmd
@@ -1,4 +1,4 @@
 @echo off
 #call bootstrap.bat
-SET ZLIB_PATH=C:\Users\Yuri\Projects\zlib-1.2.11
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --without-mpi --build-type=complete toolset=msvc-14.0 address-model=32,64 debug release install
+SET ZLIB_PATH=C:\Users\atatat\Projects\zlib-1.2.11
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --without-mpi --build-type=complete toolset=msvc-15.0 address-model=32,64 debug release install
--- boost/build_python64.cmd
+++ boost/build_python64.cmd
@@ -1,4 +1,4 @@
-SET ZLIB_PATH=C:\Projects\zlib-1.2.11
+SET ZLIB_PATH=C:\Users\atatat\Projects\zlib-1.2.11
 b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=static address-model=64 debug release install
 b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=static runtime-link=shared address-model=64 debug release install
 b2 "-sZLIB_SOURCE="%ZLIB_PATH% --with-python --prefix=C:\boost link=shared runtime-link=shared address-model=64 debug release install
