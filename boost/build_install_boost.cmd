bf98bb91bc639500a7f3562aac9d5d3b645e3c31
Yurii Cherkasov <yurii.cherkasov@protonmail.com>
2019-11-27 12:00:27 +0700

    x

--- boost/build_install_boost.cmd
+++ boost/build_install_boost.cmd
@@ -1,4 +1,10 @@
 @echo off
-#call bootstrap.bat
+
+if exist b2.exe (
+    Echo "Bjam already exist, skip bootstrap"
+) else (
+    call bootstrap.bat
+)
+
 SET ZLIB_PATH=C:\Users\atatat\Projects\zlib-1.2.11
-b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --without-mpi --build-type=complete toolset=msvc-15.0 address-model=32,64 debug release install
+b2 "-sZLIB_SOURCE="%ZLIB_PATH% --prefix=C:\boost --without-mpi --without-python --build-type=complete toolset=msvc-14.1 address-model=32,64 debug release install
