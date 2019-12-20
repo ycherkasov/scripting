4f08074c1c5090ff0da39f60b4f181ebbdab2a72
Yurii Cherkasov <yuri.cherkasov.accounts@gmail.com>
2019-12-20 15:22:53 +0700

    rename file

--- script/dictionary_merge.py
+++ script/dictionary_merge.py
@@ -1,11 +1,39 @@
 import os
 import sys
 import argparse
+import logging
+import log_helper
 import xml.etree.ElementTree as XmlTree
 import xml.dom.minidom as minidom
 
+logger = log_helper.setup_logger(name="dictionary_merge", level=logging.DEBUG, log_to_file=True)
 
-class IdeaDictionaryProvider:
+
+class BaseDictionaryProvider:
+    """
+    Base class for all dictionary providers
+    """
+    def __init__(self, file_path):
+        self.file_path = file_path
+        if not os.path.isfile(self.file_path):
+            raise RuntimeError("File does not exist: %s" % self.file_path)
+
+        self.dictionary_name = os.path.splitext(os.path.basename(self.file_path))[0]
+
+    def name(self):
+        """
+        :return: Dictionary name
+        """
+        return self.dictionary_name
+
+    def path(self):
+        """
+        :return: Path to dictionary file
+        """
+        return self.file_path
+
+
+class IdeaDictionaryProvider(BaseDictionaryProvider):
     """
     Provider class for dictionary in IDEA format.
     IDEA dictionary format is XML with pre-defined structure:
@@ -20,12 +48,16 @@ class IdeaDictionaryProvider:
     DictionaryProvider class has read() and write(new_dictionary) methods
     """
 
-    def __init__(self, xml_file_path):
-        self.xml_file_path = xml_file_path
-        if not os.path.isfile(self.xml_file_path):
-            raise RuntimeError("File does not exist: %s" % self.xml_file_path)
+    def __init__(self, file_path):
+        """
+        Open IDEA XML dictionary file
+        :param file_path: Relative or absolute path to file
+        :raise: RuntimeError if file does not exist
+        """
+        super(IdeaDictionaryProvider, self).__init__(file_path)
         self.idea_dictionary = []
-        tree = XmlTree.parse(xml_file_path)
+        logger.info("Parse dictionary %s" % self.name())
+        tree = XmlTree.parse(self.path())
         root = tree.getroot()
 
         for child in root.findall("./dictionary/words/w"):
@@ -33,7 +65,7 @@ class IdeaDictionaryProvider:
 
     def read(self):
         """
-        :return: list of dictionary items
+        :return: List of dictionary items
         """
         return self.idea_dictionary
 
@@ -48,7 +80,7 @@ class IdeaDictionaryProvider:
         doc.appendChild(component)
 
         dictionary = doc.createElement("dictionary")
-        dictionary.setAttribute("name", "atatat")
+        dictionary.setAttribute("name", self.dictionary_name)
         component.appendChild(dictionary)
 
         words = doc.createElement("words")
@@ -61,21 +93,27 @@ class IdeaDictionaryProvider:
             words.appendChild(w)
 
         xml_text = doc.toprettyxml(indent="  ")
-        with open(self.xml_file_path, "w") as idea_dict:
+        with open(self.path(), "w") as idea_dict:
             idea_dict.write(xml_text)
 
 
-class VisualAssistDictionaryProvider:
+class VAssistDictionaryProvider(BaseDictionaryProvider):
     """
     Provider class for dictionary in Visual Assist format.
     DictionaryProvider class has read() and write(new_dictionary) method.
     Visual Assist dictionary format is EOL-separated plain text
     """
 
-    def __init__(self, text_file_path):
+    def __init__(self, file_path):
+        """
+        Open VAssist plain text dictionary file
+        :param file_path: Relative or absolute path to file
+        :raise: RuntimeError if file does not exist
+        """
+        super(VAssistDictionaryProvider, self).__init__(file_path)
         self.text_dictionary = []
-        self.text_file_path = text_file_path
-        with open(text_file_path) as f:
+        logger.info("Parse dictionary %s" % self.name())
+        with open(self.path()) as f:
             self.text_dictionary = [item.strip() for item in f.readlines()]
 
     def read(self):
@@ -88,7 +126,7 @@ class VisualAssistDictionaryProvider:
         """
         :param new_dictionary: New dictionary to write, normally merged from different sources
         """
-        with open(self.text_file_path, "w") as vassist_dict:
+        with open(self.path(), "w") as vassist_dict:
             vassist_dict.write("\n".join(new_dictionary))
 
 
@@ -110,41 +148,39 @@ def main():
                         required=False)
 
     args = parser.parse_args()
-    merged_list = set()
+    all_dictionaries = []
+    merged_set = set()
 
     for idea_xml_file in args.idea_dictionary:
         idea_dict_path = os.path.abspath(idea_xml_file)
-        print("IDEA dictionary file: %s" % idea_dict_path)
+        logger.info("User IDEA dictionary file: %s" % idea_dict_path)
         if not os.path.isfile(idea_dict_path):
             return 1
-        new_dictionary = set(read_idea_dictionary(idea_dict_path))
-        print("Size of appended dictionary is %d words" % len(new_dictionary))
-        merged_list = merged_list.union(new_dictionary)
+        idea_dict = IdeaDictionaryProvider(idea_dict_path)
+        all_dictionaries.append(idea_dict)
 
     for text_file in args.text_dictionary:
-        text_file_path = os.path.abspath(text_file)
-        print("User VAssist file: %s" % text_file_path)
-        if not os.path.isfile(text_file_path):
+        vassist_file_path = os.path.abspath(text_file)
+        logger.info("User VAssist dictionary file: %s" % vassist_file_path)
+        if not os.path.isfile(vassist_file_path):
             return 1
-        new_dictionary = set(read_vassist_dictionary(text_file_path))
-        print("Size of appended dictionary is %d words" % len(new_dictionary))
-        merged_list = merged_list.union(new_dictionary)
+        vassist_dict = VAssistDictionaryProvider(vassist_file_path)
+        all_dictionaries.append(vassist_dict)
 
-    merged_list = sorted(merged_list)
-    print(merged_list)
-    print("Size of resulting dictionary is %d words" % len(merged_list))
+    logger.info("%d dictionaries to merge" % len(all_dictionaries))
 
-    print(args.idea_dictionary)
-    print(args.text_dictionary)
-    for idea_xml_file in args.idea_dictionary:
-        idea_dict_path = os.path.abspath(idea_xml_file)
-        print("Write new dictionary to %s" % idea_dict_path)
-        write_idea_dictionary(idea_dict_path, merged_list)
+    for dict in all_dictionaries:
+        new_dictionary = set(dict.read())
+        logger.info("Size of appended dictionary [%s] is %d words" % (dict.name(), len(new_dictionary)))
+        merged_set = merged_set.union(new_dictionary)
 
-    for text_file in args.text_dictionary:
-        text_file_path = os.path.abspath(text_file)
-        print("Write new dictionary to %s" % text_file_path)
-        write_vassist_dictionary(text_file_path, merged_list)
+    merged_list = sorted(merged_set)
+
+    for dict in all_dictionaries:
+        logger.info("Write new dictionary to %s" % dict.path())
+        dict.write(merged_list)
+
+    logger.info("Merge complete, size of resulting dictionary is %d words" % len(merged_list))
 
     return 0
 
